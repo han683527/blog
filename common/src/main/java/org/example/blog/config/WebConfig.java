0f1df6afa52a6ba7bf70aaf7c94e2c9a984774fc
han <han683527@qq.com>
2026-07-12 14:49:25 +0800

    feat: 增加 CORS 解决前后端跨域问题

--- common/src/main/java/org/example/blog/config/WebConfig.java
+++ common/src/main/java/org/example/blog/config/WebConfig.java
@@ -2,6 +2,7 @@ package org.example.blog.config;
 
 import org.example.blog.interceptor.TokenInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,23 +14,43 @@ public class WebConfig implements WebMvcConfigurer {
     private final TokenInterceptor tokenInterceptor;
 
     //注入
-    public WebConfig(TokenInterceptor tokenInterceptor){
+    public WebConfig(TokenInterceptor tokenInterceptor) {
         this.tokenInterceptor = tokenInterceptor;
     }
 
-    public void addInterceptors(InterceptorRegistry registry){
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenInterceptor)
-                .addPathPatterns("/**")                 //拦截所有请求
-                .excludePathPatterns("/auth/register",  //放行特定请求
-                        "/auth/login",
-                        "/auth/code",
-                        "/auth/refresh",
+                //拦截所有请求
+                .addPathPatterns("/**")
+                //排除特定请求
+                .excludePathPatterns(
+                        "/auth/**",
+                        "/article/list",
+                        "/article/{id}",
+                        "/comment/list",
+                        "/tag/**",
+                        "/category/**",
                         "/upload/**",
                         "/swagger-ui/**",
                         "/v3/api-docs/**");
     }
 
-    public void addResourceHandlers(ResourceHandlerRegistry registry){
+    // CORS 跨域处理
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 对所有接口生效
+        registry.addMapping("/**")
+                // 允许那些前端地址访问
+                .allowedOrigins("http://localhost:5173", "http://localhost:6173")
+                // 允许所有 HTTP 方法(POST, GET, PUT, DELETE)
+                .allowedMethods("*")
+                //允许所有请求头
+                .allowedHeaders("*")
+                //允许携带凭证
+                .allowCredentials(true);
+    }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**")
                 .addResourceLocations("file:D:\\java\\blog\\uploads\\");
     }
